38e8c9d734da22c916a40e104173f6eeab13ed47
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-08-31 22:44:31 +0530

    added images & json files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h2>Working</h2>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h2>Working</h2>
+    </div>
+  );
 }
 
 export default App;
